a94119417031a0dbf618dd66254553c81bffd646
Denis Udovenko <d.udovenko@7pikes.com>
2015-08-25 18:28:46 +1000

    Added AnnouncementsController and Announcement model.

---
+++ app/controllers/readit/announcements_controller.rb
@@ -0,0 +1,50 @@
+module Readit
+  # Implements controller for announcements interactions.
+  #
+  # Author:: Denis Udovenko (mailto:denis.e.udovenko@gmail.com)
+  class AnnouncementsController < ApplicationController
+    include Shared::Announcements::Cookie
+
+    # Stores given announcement id in cookie. Removes not actual announcement ids from
+    # cookie if necessary. Updated cookie expiration time.
+    def store_reviewed
+      read_announcements = extract_read_announcements
+      update_read_announcements(read_announcements)
+      store_read_announcement(read_announcements)
+
+      redirect_to :back
+    end
+
+    private
+
+    # Removes deleted announcements data from read announcements hash and adds just read
+    # announcement content digest.
+    #
+    # *Arguments*
+    # - +read_announcements+ {Hash} Extracted read announcements
+    def update_read_announcements(read_announcements)
+      existing_announcements = Announcement.where(id: read_announcements.keys)
+      existing_announcement_ids = existing_announcements.map { |announcement| announcement.id.to_s }
+      read_announcements.delete_if { |key, _| !existing_announcement_ids.include?(key) }
+      read_announcements[params[:id].to_i] = announcement_params[:content_digest]
+    end
+
+    # Writes given announcement data hash to cookie string.
+    #
+    # *Arguments*:
+    # - +read_announcements+ {Hash} Read announcements hash
+    def store_read_announcement(read_announcements)
+      cookies[Rails.configuration.cookies_keys.read_announcements] = {
+        value: read_announcements.to_json,
+        expires: Announcement.latest_stop_at(read_announcements.keys) + 1.day }
+    end
+
+    # Permits announcement parameters from received parameters hash.
+    #
+    # *Returns*
+    # {Hash} Announcement permitted parameters
+    def announcement_params
+      params.require(:announcement).permit(:content_digest)
+    end
+  end
+end
---
+++ app/models/readit/announcement.rb
@@ -0,0 +1,54 @@
+module Readit
+  # Implements site announcement model.
+  #
+  # Author:: Denis Udovenko (mailto:denis.e.udovenko@gmail.com)
+  class Announcement < ActiveRecord::Base
+    validates :content, presence: true
+    validates :start_at, presence: true
+    validates :stop_at, presence: true
+    validate :start_and_stop_datetime_order
+
+    before_save :calculate_content_digest
+
+    scope :active, proc { where(is_active: true) }
+    scope :actual, proc { where(':time_now BETWEEN start_at AND stop_at', time_now: Time.current) }
+    scope :exclude_read, proc { |read_announcements|
+      current_scope = all
+
+      read_announcements.each do |id, digest|
+        current_scope = current_scope.where('NOT (id = :id AND content_digest = :content_digest)',
+          id: id, content_digest: digest)
+      end
+
+      current_scope
+    }
+
+    # Finds latest stop time for given announcement ids enumeration.
+    #
+    # *Arguments*:
+    # - +ids+ {Array} Announcement ids
+    # *Returns*:
+    # {Datetime} Latest stop time
+    def self.latest_stop_at(ids)
+      where(id: ids).maximum(:stop_at).localtime
+    end
+
+    private
+
+    # Checks that start_at datetime is less than stop_at.
+    #
+    # *Returns*:
+    # {FlaseClass, TrueClass} False if start_at greater or equal to stop_at
+    def start_and_stop_datetime_order
+      return true if start_at < stop_at
+
+      errors.add(:base, :start_time_grater_or_equal_to_end_time)
+      false
+    end
+
+    # Calculates content digest.
+    def calculate_content_digest
+      self.content_digest = Digest::MD5.hexdigest(content)
+    end
+  end
+end
