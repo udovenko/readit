64342ed0e04b2f6222e4c526075999f3e7069d15
Denis Udovenko <d.udovenko@7pikes.com>
2015-08-28 17:35:14 +1000

    Created IndexController for dummy app. Adde Rubocop gem and rules to plugin.

---
+++ spec/dummy/app/controllers/index_controller.rb
@@ -0,0 +1,9 @@
+# Implements dummy index controller for engine tests.
+#
+# Author:: Denis Udovenko (mailto:denis.e.udovenko@gmail.com)
+class IndexController < ApplicationController
+  include Readit::Announcements
+
+  def index
+  end
+end
